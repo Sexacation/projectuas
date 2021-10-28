f901a6a9665796664d76ca3b3d1dd5df6a95ecc9
Sexacation <93278950+Sexacation@users.noreply.github.com>
2021-10-28 21:41:44 +0700

    change v1

---
+++ src/parts/IconText.js
@@ -0,0 +1,10 @@
+import React from 'react' 
+import Button from 'elements/button'
+
+export default function IconText() {
+    return (
+        <Button className="brand-text-icon" href="" type="link">
+            Sexa<span className="text-white">cation</span>
+        </Button >
+    )
+}
